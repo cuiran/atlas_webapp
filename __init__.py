0706ab845cc236fa005c83e24b914813ffc94617
cuiran <cranmail@gmail.com>
2016-11-18 14:03:43 -0500

    adding bootstrap stylesheets

--- __init__.py
+++ __init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello, I'm developing atlas webapp using Flask!"
+#def hello():
+#    return "Hello, I'm developing atlas webapp using Flask!"
+def main_page():
+    return render_template("main.html")
+
 if __name__ == "__main__":
     app.run()
