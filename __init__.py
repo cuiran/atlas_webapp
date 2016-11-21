7ee55c64c8c6840c04ea43ac4cb849d128ce357c
cuiran <cranmail@gmail.com>
2016-11-21 14:04:10 -0500

    one time communication with atlas subprocess

--- __init__.py
+++ __init__.py
@@ -1,12 +1,21 @@
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
+import subprocess
+
+# set atlas directory
+atlas_dir = "/home/ran/atlas_project/latest/atlasofliegroups/"
 
 @app.route("/")
-#def hello():
-#    return "Hello, I'm developing atlas webapp using Flask!"
 def main_page():
-    return render_template("main.html")
+    cmd = ["../atlas"]
+    p = subprocess.Popen(cmd, cwd=atlas_dir+"atlas-scripts", stdout = subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+    output,err = p.communicate()
+    return render_template("main.html", out=output)
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
 
 if __name__ == "__main__":
     app.run()
