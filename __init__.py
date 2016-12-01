470bbe99be2eeb1e1d7567009a24d55c87242c3d
cuiran <cranmail@gmail.com>
2016-12-01 13:25:50 -0500

    commented out trouble part in input_to_at function.

--- __init__.py
+++ __init__.py
@@ -8,14 +8,17 @@ import subprocess
 # set atlas directory
 atlas_dir = "/home/ran/atlas_project/latest/atlasofliegroups/"
 
+
+
 @app.route("/", methods=['GET','POST'])
 def main_page():
     cmd = ["../atlas"]
     p = subprocess.Popen(cmd, cwd=atlas_dir+"atlas-scripts", stdout = subprocess.PIPE,stderr=subprocess.PIPE,stdin=subprocess.PIPE)
     usr_request = str(request.form.get("first_options"))
     modified_input = input_to_at(usr_request)
-    output,err = p.communicate(input="<"+"\""+os.getcwd()+"/"+"input_file.at"+"\"")
-    return render_template("main.html", atlas_input=modified_input, atlas_output=trim_output(output))
+    feed_to_atlas = "<"+"\""+str(os.getcwd())+"/"+"input_file.at"+"\""
+    output,err = p.communicate(input=feed_to_atlas)
+    return render_template("main.html", atlas_input=modified_input, atlas_output=trim_output(output)+'\n'+err)
 
 @app.route('/hello')
 def hello():
