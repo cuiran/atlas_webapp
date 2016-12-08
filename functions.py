3fdb34a3cd3489406a9d1d6ec827436de26610ec
cuiran <cranmail@gmail.com>
2016-12-08 13:36:19 -0500

    atlas will start with all pre-loaded.

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
 import os
 
-def input_to_at(some_input):
+# this function takes user request and make a file in tmp folder for atlas to use
+# the return value of this function is user input formated to be displayed on the website
+def input_to_at(some_input, rand_int):
     modified_input = some_input.split('\\n')
     modified_input = "\n".join(modified_input)
-    input_file = open("/tmp/input_file.at","w")
+    input_file = open("/tmp/input_file_"+rand_int+".at","w")
     input_file.write(modified_input)
     input_file.close()
     return modified_input
