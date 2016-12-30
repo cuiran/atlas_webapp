58c3b8989a8a5c62662d60767cf36ca7dfe70e0b
cuiran <cranmail@gmail.com>
2016-12-30 15:59:32 -0500

    dynamically added content to page with json structure file

--- static/js/myjavascript.js
+++ static/js/myjavascript.js
@@ -4,12 +4,59 @@ function show(targets){
     }
 };
 
-$.getJSON('static/json/groups.json')
-	.done(function (data) {
-		var group_list = data;
-		console.log(group_list);
-		$.each(group_list, function(i,group) {
-			$('#groups').append($("<option>").attr("value", group.id).text("\$"+group.name+"\$")).selectpicker('refresh');});
-});
 
+$.getJSON('static/json/structure.json')
+	.done(function (data) {
+		var structure = data;
+		console.log(structure);
+		addTopic('#topics',structure);
+})
+
+function addTopic(appendTo, structure) {
+	$.each(structure, function(i,item) {
+		const button = $("<button>").attr({"id":item.id,"type":"button","class":"btn btn-default btn-lg"}).text(item.topic).click(clickCallback(item));
+		$(appendTo).append(button);
+	})
+}
+
+function clickCallback(item) {
+    return function() {
+        addToLastcol(item.show);
+		addToSpecify(item.specify);
+		populateGroupDropdown();
+    }
+}
+
+function addToLastcol(list) {
+	$('#lastcol').empty();
+	$('#lastcol').append("<h4>Show:</h4>");
+	$.each(list, function(i, item) {
+		const button = $("<button>").attr({"type":"button","class":"btn btn-default btn-md"}).text(item.name)
+		$('#lastcol').append(button);
+	})
+}
+
+function addToSpecify(list) {
+	$('#specify').empty();
+	$('#specify').append("<h4>Specify:</h4>");
+	$.each(list, function(i,item) {
+		console.log(item);
+		const dropdown = $("<select>").attr({"id":item, "class":"selectpicker", "data-width":"85%", "title":"pick a "+item});
+		$('#specify').append(dropdown).append("<br><br>");
+	})
+	$('.selectpicker').selectpicker('refresh');
+}
+
+// get the list of groups in groups.json and use it to populate the "pick a group" dropdown menu
+function populateGroupDropdown() {
+	$.getJSON('static/json/groups.json')
+		.done(function (data) {
+			var group_list = data;
+			console.log(group_list);
+			$.each(group_list, function(i,group) {
+				$('#group').append($("<option>").attr("value", group.id).text("\$"+group.name+"\$"));});
+			$('.selectpicker').selectpicker('refresh');
+			MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+	});
+}
 
