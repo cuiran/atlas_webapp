7d60900f7aac556dd78fb6ed234d9e3f54e04b1c
Ran Cui <cranmail@gmail.com>
2017-06-09 12:10:19 -0400

    added comments in background_process/convert_input.py

--- background_process/convert_input.py
+++ background_process/convert_input.py
@@ -15,64 +15,85 @@ def set_group(user_input):
 
 def get_atlasinput(user_input):
     if user_input['show'] == 'Cartan_Subgroups':
+        # if user request to see Cartan subgroups
         define_grp = set_group(user_input)
         define_cartans = "set cartans = Cartan_classes(G)\n"
         print_all = "for i: nr_of_Cartan_classes(G) from 0 do print(\"Cartan number \"+i); print_Cartan_info(cartans[i]) od"
         atlas_input = define_grp+define_cartans+print_all
     elif user_input['show'] == 'Real_Weyl_Group':
+        # if user request to see real Weyl group
         if user_input['further']['Cartan']!="":
+            # if user want real Weyl group but haven't input a Cartan selection yet
             define_grp = set_group(user_input)
             set_Cartan = "set H=Cartan_class(G,"+str(user_input['further']['Cartan'])+")\n"
             print_all = "print_real_Weyl(G,H)\n"
             atlas_input = define_grp+set_Cartan+print_all
         else:
+            # if user want real Weyl group and have chosen a Cartan
             define_grp = set_group(user_input)
             define_cartans = "set cartans = Cartan_classes(G)\n" 
             print_all = "for i: nr_of_Cartan_classes(G) from 0 do print(\"Cartan number \"+i); print_Cartan_info(cartans[i]) od"
             atlas_input = define_grp+define_cartans+print_all
     elif user_input['show'] == 'Real_Forms':
+        # if user want real forms
         define_grp = set_group(user_input)
         define_real_forms = "set rf = real_forms(G)\n"
         print_all = "for a in rf do prints(a) od"
         atlas_input = define_grp+define_real_forms+print_all
     elif user_input['show'] == 'Distinguished_Involution':
+        # if user want distinguished involusion
         define_grp = set_group(user_input)
         show_dist_inv = "distinguished_involution(G)\n"
         atlas_input = define_grp+show_dist_inv
     elif user_input['show'] == 'Simple_Roots':
+        # if user want simple roots
         define_grp = set_group(user_input)
         show_simple_roots = "simple_roots(G)\n"
         atlas_input = define_grp+show_simple_roots
     elif user_input['show'] == 'KGB_Elements':
+        # if user want KGB elements
         define_grp = set_group(user_input)
         print_KGB = "print_KGB(G)\n"
         print_KGB_graph = "prints(\"KGB_graph\")\nprint_KGB_graph(G)\n"
         atlas_input = define_grp+print_KGB+print_KGB_graph
     elif user_input['show'] == '':
+        # if no show element is selected
         if user_input['rep'] == 'ds':
+            # if no show element is selected and the rep selected is discrete series
             if user_input['request'] == 'is_equal_rank':
+                # if the webpage request is_equal_rank
+                # when user hasn't selected show item
+                # but has selected rep as discrete series
                 define_grp = set_group(user_input)
                 equalrk = "is_equal_rank(G)\n"
                 atlas_input = define_grp+equalrk
             elif user_input['request'] == 'rho':
+                # if the webpage request the value of rho
+                # when user hasn't selected show item
+                # but has selected rep as discrete series
                 define_grp = set_group(user_input)
                 get_rho = "rho(G)\n"
                 atlas_input = define_grp+get_rho
         elif user_input['rep'] == 'minimal_split_ps':
+            # if no show element is selected and the rep selected is minimal split ps
             if user_input['request'] == 'is_split':
+                # if webpage request value of is_split
+                # when user hasn't selected show item
+                # but has selected rep as min split ps
                 define_grp = set_group(user_input)
                 split = "is_split(G)\n"
                 atlas_input = define_grp+split
             elif user_input['request'] == 'rho':
-                define_grp = set_group(user_input)
-                get_rho = "rho(G)\n"
-                atlas_input = define_grp+get_rho
-        elif user_input['rep'] == 'minimal_split_ps':
+                # if webpage request value of rho
+                # when user hasn't selected show item
+                # but has selected rep as min split ps
                 define_grp = set_group(user_input)
                 get_rho = "rho(G)\n"
                 atlas_input = define_grp+get_rho
     elif user_input['show'] == 'information_on_parameter':
+        # if user want to see information on parameter
         if user_input['rep'] == 'ds':
+            # if user want to see information and has selected rep as discrete series
             command = "prints(\"menu_item:information_on_parameter\")\n"
             define_grp = set_group(user_input)
             if "n" in list(user_input.keys()):
@@ -92,7 +113,9 @@ def get_atlasinput(user_input):
             lkt_dim="prints(\"LKT_dimension:\", dimension(highest_weights(LKT(dsparam))[0]))\n"
             atlas_input = command + define_grp + print_grp  +  get_nice_x+ print_x_K + print_rho_K + set_dsparam + parameter+ inf_char + lkt + lkt_dim
     elif user_input['show'] == 'Branch_to_K':
+        # if user want to see branch to K
         if user_input['rep'] == 'ds':
+            #if user want to see branch to K and has selected rep as discrete series
             command = "prints(\"menu_item:Branch_to_K\")\n"
             define_grp = set_group(user_input)
             define_grp = set_group(user_input)
@@ -112,7 +135,9 @@ def get_atlasinput(user_input):
             branch = "print_branch_irr_long(dsparam,KGB(G,x_K),3*height(dsparam))\n";
             atlas_input = command+define_grp+print_grp + get_nice_x + print_x_K + print_rho_K + set_dsparam+parameter+tag+branch
     elif user_input['show'] == 'Unitarity':
+        # if user want to see unitarity
         if user_input['rep'] == 'ds':
+            # if user want to see unitarity and has selected rep as discrete series
             command = "prints(\"menu_item:unitarity\")\n"
             define_grp = set_group(user_input)
             ds_param_text = user_input['dsparam']
@@ -121,43 +146,43 @@ def get_atlasinput(user_input):
             print_rep = "dsparam\n";
             is_unitary = "is_unitary(dsparam)\n"
             atlas_input = command+ define_grp+set_dsparam+print_rep+is_unitary
-        elif user_input['rep'] == 'minimal_split_ps':
-            command = "prints(\"menu_item:unitarity\")\n"
-            define_grp = set_group(user_input)
-            ds_param_text = user_input['dsparam']
-            ds_param_asstring = make_dsparam_string(ds_param_text)
-            set_dsparam = "set dsparam=discrete_series(G,"+ds_param_asstring+")\n"
-            print_rep = "print(\"p=\",dsparam)\n";
-            is_unitary = "is_unitary(dsparam)\n"
-            atlas_input =command+ define_grp+set_dsparam+is_unitary
-    elif user_input['show'] == 'character_formula':
-        if user_input['rep'] == 'ds':
-            command = "prints(\"menu_item:character_formula\")\n"
-            define_grp = set_group(user_input)
-            define_rep = "set p=trivial(G)\n"
-            print_rep = "print(\"p=\",p)\n";
-            print_composition_series="print_character_formula(p)\n"
-            atlas_input = command+define_grp+define_rep+print_rep+print_composition_series
-        elif user_input['rep'] == 'minimal_split_ps':
-            command = "prints(\"menu_item:character_formula\")\n"
-            define_grp = set_group(user_input)
-            define_rep = "set p=trivial(G)\n"
-            atlas_input = command+define_grp+define_rep
-    elif user_input['show'] == 'composition_series':
-        if user_input['rep'] == 'ds':
-            command = "prints(\"menu_item:composition_series\")\n"
-            define_grp = set_group(user_input)
-            define_rep = "set p=trivial(G)\n"
-            print_rep = "print(\"p=\",p)\n";
-            print_composition_series="print_composition_series(p)\n"
-            atlas_input = command+define_grp+define_rep+print_rep+print_composition_series
-        elif user_input['rep'] == 'minimal_split_ps':
-            command = "prints(\"menu_item:composition_series\")\n"
-            define_grp = set_group(user_input)
-            define_rep = "set p=trivial(G)\n"
-            atlas_input = command+define_grp+define_rep
+#        elif user_input['rep'] == 'minimal_split_ps':
+#            command = "prints(\"menu_item:unitarity\")\n"
+#            define_grp = set_group(user_input)
+#            ds_param_text = user_input['dsparam']
+#            ds_param_asstring = make_dsparam_string(ds_param_text)
+#            set_dsparam = "set dsparam=discrete_series(G,"+ds_param_asstring+")\n"
+#            print_rep = "print(\"p=\",dsparam)\n";
+#            is_unitary = "is_unitary(dsparam)\n"
+#            atlas_input =command+ define_grp+set_dsparam+is_unitary
+#    elif user_input['show'] == 'character_formula':
+#        if user_input['rep'] == 'ds':
+#            command = "prints(\"menu_item:character_formula\")\n"
+#            define_grp = set_group(user_input)
+#            define_rep = "set p=trivial(G)\n"
+#            print_rep = "print(\"p=\",p)\n";
+#            print_composition_series="print_character_formula(p)\n"
+#            atlas_input = command+define_grp+define_rep+print_rep+print_composition_series
+#        elif user_input['rep'] == 'minimal_split_ps':
+#            command = "prints(\"menu_item:character_formula\")\n"
+#            define_grp = set_group(user_input)
+#            define_rep = "set p=trivial(G)\n"
+#            atlas_input = command+define_grp+define_rep
+#    elif user_input['show'] == 'composition_series':
+#        if user_input['rep'] == 'ds':
+#            command = "prints(\"menu_item:composition_series\")\n"
+#            define_grp = set_group(user_input)
+#            define_rep = "set p=trivial(G)\n"
+#            print_rep = "print(\"p=\",p)\n";
+#            print_composition_series="print_composition_series(p)\n"
+#            atlas_input = command+define_grp+define_rep+print_rep+print_composition_series
+#        elif user_input['rep'] == 'minimal_split_ps':
+#            command = "prints(\"menu_item:composition_series\")\n"
+#            define_grp = set_group(user_input)
+#            define_rep = "set p=trivial(G)\n"
+#            atlas_input = command+define_grp+define_rep
     else:
-        atlas_input = "x=10"
+        atlas_input = "set x=10"
     return atlas_input
 
 def str_to_intorfrac(s):
